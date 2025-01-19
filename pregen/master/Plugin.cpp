0572bc8e455f7e2ca3cbf1eb7fdcecedbe22ad38
falkTX <falktx@falktx.com>
2025-01-19 19:53:53 +0100

    Update pregen files
    Signed-off-by: falkTX <falktx@falktx.com>

--- pregen/master/Plugin.cpp
+++ pregen/master/Plugin.cpp
@@ -553,7 +553,7 @@ class mydsp : public dsp {
 		m->declare("basics.lib/peakholder:copyright", "Copyright (C) 2022 Dario Sanfilippo <sanfilippo.dario@gmail.com>");
 		m->declare("basics.lib/peakholder:license", "MIT-style STK-4.3 license");
 		m->declare("basics.lib/version", "0.9");
-		m->declare("compile_options", "-a /tmp/tmpct7gtc8d.cpp -lang cpp -es 1 -mcd 16 -single -ftz 0");
+		m->declare("compile_options", "-a /tmp/tmpruksqqt9.cpp -lang cpp -es 1 -mcd 16 -single -ftz 0");
 		m->declare("compressors.lib/RMS_FBFFcompressor_N_chan:author", "Bart Brouns");
 		m->declare("compressors.lib/RMS_FBFFcompressor_N_chan:license", "GPLv3");
 		m->declare("compressors.lib/RMS_compression_gain_N_chan_db:author", "Bart Brouns");
